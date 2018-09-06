7ef3d33f017115824a6fb8ee3b896a272073fb72
twototwoto <2637405168@qq.com>
2018-09-06 16:06:07 +0800

    应用中显示的文本国际化

--- WWInternationalDemo/WWInternationalDemo/ViewController.m
+++ WWInternationalDemo/WWInternationalDemo/ViewController.m
@@ -6,23 +6,20 @@
 //  Copyright © 2018年 ITCoderW. All rights reserved.
 //
 
+#define NSLocalizedString(key, comment) [[NSBundle mainBundle] localizedStringForKey:(key) value:@"" table:nil]
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(.0, .0, 200.0, 100.0)];
+    
+    [btn setTitle:NSLocalizedString(@"按钮", nil) forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+    btn.center = self.view.center;
 }
 
 
